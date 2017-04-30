6ed16574a4a0fbf4ddd9595ce7a1c1d2f4f76729
mmikolajczak <mkm0796@gmail.com>
2017-04-30 14:43:04 +0200

    - Implemented visualization part - Corrected predict method to handle set of observations instead of only one - added sample usage in main

--- perceptron.py
+++ perceptron.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# TODO: visualization part
-
 
 class LinearEquation:
     # structure that keeps parameters of linear equation
@@ -12,8 +10,12 @@ class LinearEquation:
         self.b = b
         self.x0 = - b / a
 
+    def count(self, x):
+        # return value of function for provided x
+        return self.a * x + self.b
 
-def generate_sample(lin_eq, range_start=-1000, range_end=1000):
+
+def generate_sample(lin_eq, range_start=-50, range_end=50):
     # generates sample point with x, y coordinates in specified range
     # additionally returns proper classification for a point: 1 if it is
     # above line specified by lin_eq parameter and -1 if it lies below
@@ -21,14 +23,6 @@ def generate_sample(lin_eq, range_start=-1000, range_end=1000):
     cord_y = np.random.uniform(range_start, range_end)
     result = 1 if cord_y >= lin_eq.a * cord_x + lin_eq.b else -1
     return np.array([cord_x, cord_y, result])
-
-
-def draw_plot(orig_a, orig_b, pred_a, pred_b):
-    # for future visualization purposes
-    plt.plot([-10, 10], [-10 * orig_a + orig_b, 10 * orig_a + orig_b], c='red')
-    plt.plot([-10, 10], [-10 * pred_a + pred_b, 10 * pred_a + pred_b], c='blue')
-    plt.title('Visualization test')
-    plt.show()
 
 
 class SimplePerceptron:
@@ -43,32 +37,103 @@ class SimplePerceptron:
         self._bias = 0
         self._eta = eta
 
-    def predict(self, inputs):
+    def predict(self, X_test):
         # predict value for provided inputs (that should be passed as np array)
-        if self._weights.dot(inputs) + self._bias >= 0:
-            return 1
-        else:
-            return -1
+        predictions = []
+        for idx in range(X_test.shape[0]):
+            if self._weights.dot(X_test[idx, :]) + self._bias >= 0:
+                predictions.append(1)
+            else:
+                predictions.append(-1)
+
+        return np.array(predictions).reshape((-1, 1))
 
     def fit(self, X_train, y_train):
         # trains perceptron from provided training data, X_train is a matrix of observations,
         # while y_train is vector of correct result for each observation
         for idx in range(X_train.shape[0]):
-            y_pred = self.predict(X_train[idx, :])
-            self._weights += self._eta * X_train * (y_train[idx] - y_pred)
+            y_pred = self.predict(X_train[idx, :].reshape((1, -1)))
+            self._weights += self._eta * X_train[idx, :] * (y_train[idx] - y_pred)
             self._bias += self._eta * (y_train[idx] - y_pred)
 
 
 def benchmark(lin_eq, predictor, n_tests=1000):
     # tests trained predictor, lin_eq that predictor was trained against needs to
     # be provided, tests quantity specified by n_tests
-    errors = 0
+
+    correctly_classified = []
+    wrong_classified = []
+
     for i in range(n_tests):
         sample = generate_sample(lin_eq)
-        if sample[2] != predictor.predict(sample[:2]):
-            errors += 1
+        if sample[-1] != predictor.predict(sample[:-1].reshape((1, -1))):
+            wrong_classified.append(sample[:-1])
+        else:
+            correctly_classified.append(sample[:-1])
 
-    return errors, errors / 1000
+    return np.array(correctly_classified), np.array(wrong_classified)
+
+
+class TestStats:
+    # structure to keep test results and some statistics about them
+
+    def __init__(self, errors, total):
+        self.errors = errors
+        self.total = total
+        self.error_rate = errors / total
+        self.accuracy = (1 - self.error_rate) * 100
+
+
+def count_statistics(correctly_classified, wrongly_classified):
+    # return TestStats object from data about correctly and wrongly classified points
+    return TestStats(wrongly_classified.shape[0], correctly_classified.shape[0] + wrongly_classified.shape[0])
+
+
+def visualize_training(lin_eq, predictor, n_iterations, samples_per_iteration):
+    # On each iteration it visualizes results of prediction for a few samples, at the end of iteration
+    # it predictor uses this data again to learn from it
+    # We move on to next iteration by simple mouse button click
+
+    for i in range(n_iterations):
+
+        # generating set of samples
+        for j in range(samples_per_iteration):
+            if j == 0:
+                samples = generate_sample(lin_eq)
+                continue
+            new_sample = generate_sample(lin_eq)
+            samples = np.vstack([samples, new_sample])
+
+        # predict and count divide good/bad predictions
+        predictions = predictor.predict(samples[:, :-1])
+        correctly_classified = []
+        wrongly_classified = []
+
+        for j in range(predictions.shape[0]):
+            if predictions[j, -1] != samples[j, -1]:
+                wrongly_classified.append(samples[j, :-1])
+            else:
+                correctly_classified.append(samples[j, :-1])
+
+        correctly_classified = np.array(correctly_classified)
+        wrongly_classified = np.array(wrongly_classified)
+
+        # visualization itself
+        if correctly_classified.shape[0] != 0:
+            plt.scatter(correctly_classified[:, 0], correctly_classified[:, 1], c='green')
+        if wrongly_classified.shape[0] != 0:
+            plt.scatter(wrongly_classified[:, 0], wrongly_classified[:, 1], c='red')
+        plt.plot([-50, 50], [lin_eq.count(-50), lin_eq.count(50)])
+        plt.title('Iteration nr: {}, Sample size: {}'.format(i + 1, samples_per_iteration))
+        plt.xlabel('X')
+        # plt.xlim(-55, 55)
+        plt.ylabel('Y')
+        plt.pause(0.05)
+        plt.waitforbuttonpress()
+        plt.clf()
+
+        # predictor learns from data it predicted earlier
+        predictor.fit(samples[:, :-1], samples[:, -1])
 
 
 def main():
@@ -79,21 +144,18 @@ def main():
     lin_eq = LinearEquation(a=2.3, b=-0.4)
 
     # number of training iterations
-    iterations = 10 ** 5
+    iterations = 20
 
     sp = SimplePerceptron(eta=0.01)
 
-    for i in range(iterations):
-        sample = generate_sample(lin_eq)
-        X_train = np.array([sample[0:2]]).reshape((1, 2))
-        y_train = np.array([sample[2]]).reshape((1, 1))
-        sp.fit(X_train, y_train)
-        # if i % 10000 == 0:
-        # draw_plot(a, b, sn._weights[0], sn._bias)
+    visualize_training(lin_eq, sp, iterations, samples_per_iteration=100)
 
-    errors, error_rate = benchmark(lin_eq, sp)
-    print('Total errors after training:', errors)
-    print('Error rate after training:', error_rate)
-    print('Accuracy: {}%'.format((1 - error_rate) * 100))
+    # Benchmark after training
+    correctly_classified, wrongly_classified = benchmark(lin_eq, sp)
+    test_stats = count_statistics(correctly_classified, wrongly_classified)
+
+    print('Total errors after training:', test_stats.errors)
+    print('Error rate after training:', test_stats.error_rate)
+    print('Accuracy: {}%'.format(test_stats.accuracy))
 
 main()
